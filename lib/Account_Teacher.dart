0609937559dd4c223ca3a4ba5ea6db6a235167df
Thomaslgts <74049629+Thomaslgts@users.noreply.github.com>
2021-05-17 15:18:43 +0200

    Update Account_Teacher.dart
    Mise en place du package album.dart

--- lib/Account_Teacher.dart
+++ lib/Account_Teacher.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projetuto/model/album.dart';
 
 class ProfileTeacher extends StatefulWidget {
   @override
@@ -6,26 +7,29 @@ class ProfileTeacher extends StatefulWidget {
 }
 
 class _ProfileTeacherState extends State<ProfileTeacher> {
+  Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[800],
-      body: Column(
-        children: [
-          SafeArea(
-            child: Container(
-              margin: EdgeInsets.only(top: 40),
-              child: Center(
-                child: Text('Bienvenue dans votre espace professeur',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 25, color: Colors.white)),
-              ),
-            ),
-          ),
-          Text('Vous avez 1 test en attente',
-              style: TextStyle(fontSize: 25, color: Colors.white)),
-        ],
-      ),
-    );
+        backgroundColor: Colors.blue[900],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Espace professeur'),
+        ),
+        body: Center(
+            child: FutureBuilder<Album>(
+                future: futureAlbum,
+                builder: (context, snapshot) {
+                  return Column(
+                    children: [Text("Bonjour")],
+                  );
+                })));
   }
 }
