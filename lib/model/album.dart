97bfeef238448ec8a56ce7cbe1848e699be971ec
CostierLucas <lucas.costier@outlook.fr>
2021-06-04 17:17:48 +0200

    serveur en ligne

--- lib/model/album.dart
+++ lib/model/album.dart
@@ -4,7 +4,8 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 
 Future<Album> fetchAlbum() async {
-  final response = await http.get(Uri.http('localhost:8080', 'api/quiz/321'));
+  final response =
+      await http.get(Uri.http('serveur-flutter.herokuapp.com', 'api/quiz/321'));
   final jsonresponse = json.decode(response.body);
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
